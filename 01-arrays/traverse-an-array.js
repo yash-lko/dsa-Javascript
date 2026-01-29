82ea3b88845e2565d3759d938455d501fba381ee
yashwant <yashwantbit@gmail.com>
2026-01-29 15:06:57 +0530

    Traverse an Array

---
+++ 01-arrays/traverse-an-array.js
@@ -0,0 +1,19 @@
+
+
+// Time Complexity- O(n)-loop through each element once.
+// Space Complexity- O(1)-not creating any extra arrays or objects.
+
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+function traverse(nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+
+       console.log(nums[i])
+    }
+
+}
+
+traverse(arr)
+
