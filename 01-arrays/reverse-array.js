d55d66d1625d20b0e8b7c1e7752a60fa97d29eb5
yashwant <yashwantbit@gmail.com>
2026-01-29 15:39:09 +0530

    Reverse an array

--- 01-arrays/reverse-array.js
+++ 01-arrays/reverse-array.js
@@ -1,5 +1,16 @@
 
+// TC - O(n)
+// SC- O(n)
 
-const arr=[1,2,3,4,5,6]
+const arr = [1, 2, 3, 4, 5, 6]
+
+function reverse(nums) {
+    let reversed = []
+    for (let i = nums.length - 1; i >= 0; i--) {
+        reversed.push(nums[i])
+    }
+    return reversed
+}
+console.log(reverse(arr))    // [6, 5, 4, 3, 2, 1]
 
 
