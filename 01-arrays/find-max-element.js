fa10bbd20486c34b48a067e579328d1373a692e5
yashwant <yashwantbit@gmail.com>
2026-01-30 10:45:06 +0530

    Add find max element solution

---
+++ 01-arrays/find-max-element.js
@@ -0,0 +1,19 @@
+
+// Time Complexity- O(n)
+//Space Complexity - O(1) Only constant variables
+
+const arr = [1, 2, 3, 6, 4, 1]
+
+function maxfun(nums) {
+
+    let max = nums[0]
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            max = nums[i]
+        }
+    }
+    return max
+}
+console.log(maxfun(arr))   //  output-   6
+
