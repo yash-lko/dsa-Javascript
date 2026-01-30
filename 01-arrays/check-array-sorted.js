73e96ece781b850ba283f993246a845905327185
yashwant <yashwantbit@gmail.com>
2026-01-30 15:11:45 +0530

    Check if array is sorted both acs & desc

--- 01-arrays/check-array-sorted.js
+++ 01-arrays/check-array-sorted.js
@@ -8,10 +8,6 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]  //ascending
 const arr2 = [9, 8, 7, 6, 5, 4, 3, 2, 1] //descending
 
 function myfun(nums) {
-
-
-    console.log(nums.length)
-
     for (let i = 0; i < nums.length - 1; i++) {
 
         if (nums[i] > nums[i + 1]) {
