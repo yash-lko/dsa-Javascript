3e9a5f251b376b00fbd073249311751a300b4884
yashwant <yashwantbit@gmail.com>
2026-01-31 21:33:12 +0530

    Move Zeros to end

--- 01-arrays/two-pointers/move-zeros-to-end.js
+++ 01-arrays/two-pointers/move-zeros-to-end.js
@@ -1,22 +1,45 @@
 // Time Complexity- O(n)
 //Space Complexity- O(n)
 
+const arr = [1, 0, 3, 0, 4, 5, 6, 0]
 
-
-
-const arr= [1,0,3,0,4,5,6, 0]
-
-function myFun(nums){
-    let nonzeros=[]
-    let zeros=[]
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]!==0){
+function myFun(nums) {
+    let nonzeros = []
+    let zeros = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
             nonzeros.push(nums[i])
-        }else{
+        } else {
             zeros.push(nums[i])
         }
     }
-   
-  return [...nonzeros, ...zeros]
+
+    return [...nonzeros, ...zeros]
 }
 console.log(myFun(arr))   // [1, 3, 4, 5, 6, 0, 0, 0]
+
+
+
+
+
+// Little Optimal way
+
+// function myFun(nums) {
+//     let nonzeros = []
+//     let zeros = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== 0) {
+//             nonzeros.push(nums[i])
+//         } else {
+//             zeros++
+//         }
+//     }
+
+//     while(zeros--) {
+//         nonzeros.push(0)
+//     }
+//     return nonzeros;
+// }
+
+
+   //console.log(myFun(arr))  // [1, 3, 4, 5, 6, 0, 0, 0]
