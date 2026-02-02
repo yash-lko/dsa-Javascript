f500bc0459aca62af49f41211cb37ad5d9af5e23
yashwant <yashwantbit@gmail.com>
2026-02-02 17:34:42 +0530

    Two Pointers

--- 01-arrays/two-pointers/check-two-nums-sum-to-10.js
+++ 01-arrays/two-pointers/check-two-nums-sum-to-10.js
@@ -1,50 +1,58 @@
 
 //Given an array of numbers and a target value, check whether any two numbers add up to the target.
 
+//  Only for sorted array
 
 //Two Pointers solution
 
-// function myFun(){
+//Time Complexity - O(n)
+//Space Complexity - O(1)
 
-// }
+const arr = [1, 3, 4, 5, 7];
+const target = 10;
+function myFun(nums, tar) {
 
+    let slow = 0;
+    let fast = nums.length - 1;
+    while (slow < fast) {
 
+        let sum = nums[slow] + nums[fast];
 
+        if (sum === tar) {
+            return true;
+        } else if (sum > tar) {
+            fast--
+        } else {
+            slow++
+        }
 
-
-
-
-
-
-
-
-
-
-
+    }
+    return false;
+}
+console.log(myFun(arr, target))     //true
 
 
 // Time Complexity - O(n²)
 // Space Complexity- O(1)
 
 //Brute force solution
-const arr = [1, 3, 4, 5, 7];
-const target = 10;
 
-function myFun(nums, tar) {
 
-    for (let i = 0; i < nums.length; i++) {
+// function myFun(nums, tar) {
 
-        for (let j = i + 1; j < nums.length; j++) {
+//     for (let i = 0; i < nums.length; i++) {
 
-            if (nums[i] + nums[j] === tar) {
+//         for (let j = i + 1; j < nums.length; j++) {
 
-                return true
-            }
-        }
-    }
-    return false
-}
-console.log(myFun(arr, target))   // true
+//             if (nums[i] + nums[j] === tar) {
+
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// }
+// console.log(myFun(arr, target))   // true
 
 
 
