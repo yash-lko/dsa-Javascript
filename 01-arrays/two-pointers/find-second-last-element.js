37ae5d93ebd090c75d88469c059670b3a606a8b0
yashwant <yashwantbit@gmail.com>
2026-02-08 20:25:53 +0530

    Two Pointers

--- 01-arrays/two-pointers/find-second-last-element.js
+++ 01-arrays/two-pointers/find-second-last-element.js
@@ -11,12 +11,23 @@ const arr = [1, 2, 8, 4, 3, 5]
 // Space Complexity - O(n)
 function myFun(nums) {
 
-    let max = Math.max(...nums);
-    let indexOfMax = nums.indexOf(max);
-    nums.splice(indexOfMax, 1)
-    let secLargest = Math.max(...nums);
-    return secLargest
+    // let max = Math.max(...nums);
+    // let indexOfMax = nums.indexOf(max);
+    // nums.splice(indexOfMax, 1)
+    // let secLargest = Math.max(...nums);
+    // return secLargest      
 
+               // Working but not safe method, because it changes the original array
+
+
+    let temp=[...nums];    
+    let max=Math.max(...temp);  
+    let index=temp.indexOf(max)
+    temp.splice(index, 1)
+      return Math.max(...temp)
+
+    console.log(temp)
+   
 
 }
 
