40258b36e3b2088d53d7bb55db98c6874b31fa76
Yashwant Nishad <53366902+yash-lko@users.noreply.github.com>
2026-02-10 22:38:42 +0530

    Update check-array-sorted.js

--- 01-arrays/check-array-sorted.js
+++ 01-arrays/check-array-sorted.js
@@ -1,8 +1,6 @@
 
 // Time Complexity= O(n)
-// Space Complexity= 
-
-
+// Space Complexity= O(1)
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]  //ascending
 const arr2 = [9, 8, 7, 6, 5, 4, 3, 2, 1] //descending
