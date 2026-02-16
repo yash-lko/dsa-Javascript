244b54b0e046a30f0b599962ae10288bfbcfda7b
yashwant <yashwantbit@gmail.com>
2026-02-16 15:46:07 +0530

    two pointers

--- 01-arrays/two-pointers/rotate-array-by-1-left-and-right.js
+++ 01-arrays/two-pointers/rotate-array-by-1-left-and-right.js
@@ -2,40 +2,40 @@ const arr=[1, 2, 4,3 ];
 
 // Rotate array by 1 (left)
 
-function myFun(nums){
-  
-  let temp=nums[0]
-  
-  for(let i=1; i<nums.length; i++){
-    
-    nums[i-1]=nums[i]
-    
-  }
-nums[nums.length-1]=temp;
-
-return nums
-  
-}
-console.log(myFun(arr)) // [2,4,3,1]
-
-
-
-
-// Rotate array by 1 (Right)
 // function myFun(nums){
   
-//   let temp= nums[nums.length-1]
-//   console.log(temp, "temp")
+//   let temp=nums[0]
   
-//   for(let i=nums.length-1; i<=0; i--){
-     
-//     nums[i+1]=nums[i] //1:1, 2:2, 3:4, 
+//   for(let i=1; i<nums.length; i++){
+    
+//     nums[i-1]=nums[i]
+    
 //   }
-//    nums[0]=temp
-   
-//   return nums;
+// nums[nums.length-1]=temp;
 
+// return nums
   
 // }
-// console.log(myFun(arr)) // [3,1,2,4]
+// console.log(myFun(arr)) // [2,4,3,1]
+
+
+
+
+//Rotate array by 1 (Right)
+function myFun(nums){
+  
+  let temp= nums[nums.length-1]
+  console.log(temp, "temp")
+  
+  for(let i=nums.length-1; i<=0; i--){
+     
+    nums[i+1]=nums[i] //1:1, 2:2, 3:4, 
+  }
+   nums[0]=temp
+   
+  return nums;
+
+  
+}
+console.log(myFun(arr)) // [3,1,2,4]
 
