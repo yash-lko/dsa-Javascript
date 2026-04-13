8e2b18c31565d1eb1379629c31d246a6f5786b2e
yashwantnishad-max <yashwant.nishad@silverpush.co>
2026-04-13 13:15:26 +0530

    Merge two arrays

---
+++ 01-arrays/imp-questions/merge-two-array-two-pointers.js
@@ -0,0 +1,33 @@
+
+//Merge two sorted arrays into one sorted array (using the two pointers technique
+let arr1 = [1, 3, 5]
+let arr2 = [2, 4, 6, 8]
+
+function mergeArr(num1, num2) {
+
+    let merged = []
+    let i = 0; j = 0;
+
+    while (i < num1.length && j < num2.length) {
+
+        if (num1[i] < num2[j]) {
+            merged.push(num1[i])
+            i++
+        } else {
+            merged.push(num2[j])
+            j++
+        }
+
+    }
+    while (i < num1.length) {
+        merged.push(num1[i])
+        i++
+    }
+    while (j < num2.length) {
+        merged.push(num2[j])
+        j++
+    }
+    return merged
+
+}
+console.log(mergeArr(arr1, arr2))  // output [1,2,3,4,5,6,8]
